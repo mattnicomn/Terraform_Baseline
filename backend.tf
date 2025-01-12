0e36180bb80a4412201888820e3ba3e319214dd4
mattnicomn <mattnicomn10@gmail.com>
2025-01-12 15:13:55 -0500

    update04

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket"
-    key            = "Terraform_Baseline/terraform.tfstate"
+##    key            = "Terraform_Baseline/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock-table"
     acl            = "private"
