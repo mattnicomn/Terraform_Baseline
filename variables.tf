71d362fb031705903bbcd280581e1d7669cbb76a
mattnicomn <mattnicomn10@gmail.com>
2025-01-12 14:55:19 -0500

    update-03

--- variables.tf
+++ variables.tf
@@ -3,3 +3,15 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "aws_access_key" {
+  description = "AWS region for deployment"
+  type        = string
+  default     = "AKIATWHEHEZE5Y5TVOE6"
+}
+
+variable "aws_secret_key" {
+  description = "AWS region for deployment"
+  type        = string
+  default     = "qksW8ZzgqLef6/m48aMPZCANUZNwCuHEXrLcYDL+"
+}
