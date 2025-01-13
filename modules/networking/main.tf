4152b450ccf782148601f7919878546603f2af11
mattnicomn <mattnicomn10@gmail.com>
2025-01-12 19:50:08 -0500

    update-net-11

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -5,13 +5,20 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_subnet" "subnets" {
-  count           = length(var.subnet_cidrs)
-  vpc_id          = aws_vpc.main.id
-  cidr_block      = var.subnet_cidrs[count.index]
-  availability_zone = data.aws_availability_zones.available.names[count.index]
-  tags = {
-    Name = "Subnet ${count.index}"
-  }
+#resource "aws_subnet" "subnets" {
+#  count           = length(var.subnet_cidrs)
+#  vpc_id          = aws_vpc.main.id
+#  cidr_block      = var.subnet_cidrs[count.index]
+#  availability_zone = data.aws_availability_zones.available.names[count.index]
+#  tags = {
+#    Name = "Subnet ${count.index}"
+#  }
+#}
+
+resource "aws_subnet" "example" {
+  for_each           = toset(var.aws_availability_zones)
+  vpc_id             = var.vpc_id
+  cidr_block         = cidrsubnet(var.cidr_block, 8, index(var.aws_availability_zones, each.value))
+  availability_zone  = each.value
 }
 
