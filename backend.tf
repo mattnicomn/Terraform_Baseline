7e7d3210245ed52af087fe9139b43a44ec870f72
mattnicomn <mattnicomn10@gmail.com>
2025-01-26 09:15:28 -0500

    update-net-subnet-10

--- backend.tf
+++ backend.tf
@@ -14,4 +14,6 @@ terraform {
   }
 }
 
-
+data "aws_ssm_parameter" "latest_ami" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
--- main.tf
+++ main.tf
@@ -34,7 +34,5 @@ module "storage" {
   source = "./modules/storage"
 }
 
-data "aws_ssm_parameter" "latest_ami" {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
-}
+
 
