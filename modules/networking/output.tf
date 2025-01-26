261786c44f050bf4bcd2c390ef6d6ced87d69373
mattnicomn <mattnicomn10@gmail.com>
2025-01-26 08:07:57 -0500

    update-net-vpc-02

--- modules/networking/output.tf
+++ modules/networking/output.tf
@@ -3,7 +3,13 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
+#output "subnet_ids" {
+#  value = aws_subnet.subnets[*].id
+#}
+
 output "subnet_ids" {
-  value = aws_subnet.subnets[*].id
+  description = "The IDs of the subnets created"
+  value       = values(aws_subnet.subnets)[*].id
 }
+
 
