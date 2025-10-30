0c8c5b1bf0826f2ff0fe9554bf4967908b976bc6
mattnicomn <mattnicomn10@gmail.com>
2025-10-29 20:22:40 -0400

    remove keys

--- variables.tf
+++ variables.tf
@@ -7,12 +7,9 @@ variable "aws_region" {
 variable "aws_access_key" {
   description = "AWS region for deployment"
   type        = string
-  default     = "AKIATWHEHEZE5Y5TVOE6"
 }
 
 variable "aws_secret_key" {
   description = "AWS region for deployment"
   type        = string
-  default     = "qksW8ZzgqLef6/m48aMPZCANUZNwCuHEXrLcYDL+"
 }
-
